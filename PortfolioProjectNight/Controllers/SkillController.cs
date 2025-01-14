d441f1fed40a57bb06f14671307717b3fe819ad8
m.anilcelebii@outlook.com <m.anilcelebii@outlook.com>
2025-01-14 14:34:05 +0300

    ..

---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,50 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNighttEntities context = new DbMyPortfolioNighttEntities();
+        
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+    }
+}
